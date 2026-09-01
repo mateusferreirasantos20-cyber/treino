db2362e795c9429065f5e9e8c7c943b32a024b82
mateusferreirasantos111 <mateus.ferreira.santos20@escola.pr.gov.br>
2026-09-01 11:39:36 -0300

    3

--- bl.py
+++ bl.py
@@ -17,3 +17,9 @@ print(idade)
 mensagem = "What are you doing?"
 print("Hello man,", mensagem)
 
+idade = 15
+if idade >= 15:
+    print=("You are of the required age")
+else:
+    print=("You don't meet the age requirement")
+
